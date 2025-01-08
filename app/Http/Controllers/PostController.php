c267fd024837e91471221f236ded744e7a247d10
Kaspars <kkcdp@inbox.lv>
2025-01-08 17:44:19 +0200

    project start

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+
+    {
+        $posts = Post::all();
+        return Inertia::render('Posts/Index', [
+            'posts' => $posts
+        ]);
+    }
+}
---
+++ resources/js/Pages/Posts/Index.jsx
@@ -0,0 +1,36 @@
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
+
+export default function Dashboard({auth, posts}) {
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            header={
+                <h2 className="text-xl font-semibold leading-tight text-gray-800">
+                    Posts
+                </h2>
+            }
+        >
+            <Head title="Posts">
+                <meta name='description' content='Posts Index'/>
+            </Head>
+
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+
+                    {posts.map((post)=>{
+                        return(
+                            <div key={post.id} className="p-6 text-gray-900">
+                            {post.body}
+                            </div>
+                        )
+                    })}
+
+                        
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
